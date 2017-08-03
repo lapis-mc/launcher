17314cc9f261f8f365615293d3400948f0c52822
Michael Miller <icy.arctic.fox@gmail.com>
2017-08-03 10:32:46 -0600

    Add data class for representing installed versiosn of Java

---
+++ core/src/main/kotlin/com/lapismc/lapis/launcher/core/SystemJava.kt
@@ -0,0 +1,40 @@
+package com.lapismc.lapis.launcher.core
+
+import com.lapismc.minecraft.versioning.OSType
+import oshi.SystemInfo
+
+/**
+ * Information about a version of Java installed on the system.
+ * @param version Java version string (e.g. 1.8.0_144).
+ * @param path Absolute path to the Java installation.
+ * @param is64bit Flag indicating whether Java is 64-bit.
+ */
+data class SystemJava(val version: String, val path: String, val is64bit: Boolean) {
+    /**
+     * Bytes in a gigabyte, used to syntactical conversion.
+     */
+    private val GB = (1024 * 1024 * 1024).toLong()
+
+    /**
+     * Calculates the maximum amount of memory this installation of Java can use.
+     * @return Memory size in bytes.
+     */
+    fun maximumPossibleMemory(): Long {
+        val sysInfo = SystemInfo()
+        val totalSysMemory = sysInfo.hardware.memory.total
+        return if(is64bit && sysInfo.hardware.processor.isCpu64bit)
+            totalSysMemory
+        else
+            Math.min(totalSysMemory, osMaxHeap())
+    }
+
+    /**
+     * Recommended maximum memory Java should use for a 32-bit environment.
+     * @return Memory size in bytes.
+     */
+    private fun osMaxHeap() = when(OSType.current) {
+        OSType.LINUX   -> 3 * GB
+        OSType.OSX     -> 4 * GB
+        OSType.WINDOWS -> 2 * GB
+    }
+}
