110b5ede6faf39a7600a47cee44a306598f8df45
Michael Miller <icy.arctic.fox@gmail.com>
2018-03-25 15:51:14 -0600

    Make modded types abstract
    This is because there could be different implementations for installing modded Minecraft.

--- core/src/main/kotlin/com/lapismc/lapis/launcher/core/ModdedInstaller.kt
+++ core/src/main/kotlin/com/lapismc/lapis/launcher/core/ModdedInstaller.kt
@@ -1,6 +1,5 @@
 package com.lapismc.lapis.launcher.core
 
-import com.github.kittinunf.result.Result
 import com.lapismc.minecraft.versioning.MetaService
 
 /**
@@ -8,21 +7,5 @@ import com.lapismc.minecraft.versioning.MetaService
  * @param metaService Service used to retrieve file content.
  * @param bundle Package to install.
  */
-internal class ModdedInstaller(metaService: MetaService, private val bundle: Bundle)
-    : Installer(metaService) {
-    /**
-     * Performs the installation of an instance to a store.
-     * @param store Instance storage to install to.
-     * @return Result of the installation.
-     *  Upon success, an instance is returned.
-     */
-    override fun install(store: InstanceStore): Result<InstalledInstance, Exception> = TODO()
-
-    /**
-     * Checks that the package is properly installed to the instance storage.
-     * @param store Instance storage to install to.
-     * @return Result of the verification.
-     *  If any problems were found, the result will be a failure.
-     */
-    override fun verify(store: InstanceStore): Result<InstalledInstance, Exception> = TODO()
-}
+internal abstract class ModdedInstaller(metaService: MetaService, private val bundle: Bundle)
+    : Installer(metaService)
--- core/src/main/kotlin/com/lapismc/lapis/launcher/core/ModdedInstance.kt
+++ core/src/main/kotlin/com/lapismc/lapis/launcher/core/ModdedInstance.kt
@@ -1,34 +1,14 @@
 package com.lapismc.lapis.launcher.core
 
-import com.github.kittinunf.result.Result
-import com.lapismc.minecraft.versioning.MetaService
-
 /**
  * References a single installation of modded Minecraft.
  * @param minecraftVersionId Version ID string of Minecraft.
  * @param javaConfiguration Java configuration used to launch the game.
  */
-class ModdedInstance(minecraftVersionId: String, javaConfiguration: JavaConfiguration)
+abstract class ModdedInstance(minecraftVersionId: String, javaConfiguration: JavaConfiguration)
     : Instance(minecraftVersionId, javaConfiguration) {
     /**
      * Indicates whether the instance uses mods.
      */
     override val isModded = true
-
-    /**
-     * Creates an installer that can install the instance to a file system.
-     * @param metaService Service used to retrieve files.
-     * @return Installer to create the instance.
-     */
-    override fun createInstaller(metaService: MetaService): Result<Installer, Exception> = Result.Success(ModdedInstaller(metaService, generatePackage(metaService)))
-
-    /**
-     * Constructs the package for the installer.
-     * @param metaService Service used to retrieve files.
-     * @return Package to install the instance.
-     */
-    private fun generatePackage(metaService: MetaService): Bundle {
-        val builder = Bundle.Builder()
-        return builder.build()
-    }
 }
